b5c7d12de7bb4d5956c591e346c7384cf141090f
Darrow Palast <dpalast@gmail.com>
2019-08-02 20:19:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -64,6 +64,7 @@ def dennis_ritchies_language
   programmer_hash[:dennis_ritchie][:languages][0]
 end
 
+
 def adding_matz
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
@@ -87,7 +88,10 @@ def adding_matz
         }
      }
 
-    
+  programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+  }
 end
 
 def changing_alan
